257195e521142f5332d9f945be6fa7429e72b166
1081582 <1081582@hr.nl>
2026-03-18 12:48:27 +0100

    Build and run script

---
+++ buildAndRun.ps1
@@ -0,0 +1,10 @@
+cd ./src
+
+echo "Building program"
+emcc simulator_web_interface.c -o simulator_web_interface.js -O2 -s EXPORTED_FUNCTIONS='["_simulate", "_set_pilot_input", "_retrieve_state_variable", "_retrieve_simulation_time", "_main"]' -s WASM=1 -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' 
+echo "Program complete"
+
+cd ./..
+
+echo "starting server"
+Start-Process powershell.exe "./startServer.ps1"
